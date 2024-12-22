1aaa05cc3192c4fc6866cdb7aaa8fd1bf771eded
xchellx <jon.xjonx.nat@gmail.com>
2024-12-22 13:12:28 -0500

    Update submodules, fix git version, and fix YAML deserialization writing/reading duplicate key based on inheritence

--- BNRSharp/Serialization/YAML/AYAMLBNR.cs
+++ BNRSharp/Serialization/YAML/AYAMLBNR.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using YamlDotNet.Serialization;
 
 namespace BNRSharp.Serialization.YAML
 {
     public abstract class AYAMLBNR : IYAMLBNR
     {
         [NotNull]
+        [YamlIgnore]
         public abstract bool? IsBNR2_V { get; set; }
 
+        [YamlIgnore]
         public bool IsBNR2 { get => IsBNR2_V.Value; set => IsBNR2_V = value; }
     }
 }
